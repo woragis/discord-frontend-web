ea29db5747c49f0d8fcbbdde6a570ca21f779b54
Woragis <masteringthecode.woragis+github@gmail.com>
2023-10-30 00:50:18 -0300

    Change global styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -6,4 +6,24 @@ export const globalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 }
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #313338;
+  color: #f2f3f5;
+  /* color: #797C86; */
+  /* color: #219E56; */
+  /* color: #1E1F22; */
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
+
 `;
